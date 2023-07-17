5ad6d830bdae682b5be162b118b78659e58599d9
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2023-07-17 11:57:31 +0200

    Always overwrite config

--- mediawiki/Quickstatements.sh
+++ mediawiki/Quickstatements.sh
@@ -4,16 +4,15 @@ set -ex
 php /var/www/html/extensions/OAuth/maintenance/createOAuthConsumer.php \
     --approve \
     --callbackUrl  "$QS_PUBLIC_SCHEME_HOST_AND_PORT/api.php" \
-    --callbackIsPrefix true --user "$MW_ADMIN_NAME" --name QuickStatements --description QuickStatements --version 1.0.1 \
+    --callbackIsPrefix true --user "$MW_ADMIN_NAME" --name QuickStatements --description QuickStatements --version `date '+%Y%m%d%H%M%S' \
     --grants createeditmovepage --grants editpage --grants highvolume --jsonOnSuccess > /quickstatements/data/qs-oauth.json \
     --conf /var/www/html/LocalSettings.php
 
-if [[ -f /quickstatements/data/qs-oauth.json ]]; then
-    OAUTH_CONSUMER_KEY=$(grep -o '"key":"[^"]*' /quickstatements/data/qs-oauth.json | grep -o '[^"]*$')
-    OAUTH_CONSUMER_SECRET=$(grep -o '"secret":"[^"]*' /quickstatements/data/qs-oauth.json | grep -o '[^"]*$')
-	
-    export OAUTH_CONSUMER_KEY
-    export OAUTH_CONSUMER_SECRET
+OAUTH_CONSUMER_KEY=$(grep -o '"key":"[^"]*' /quickstatements/data/qs-oauth.json | grep -o '[^"]*$')
+OAUTH_CONSUMER_SECRET=$(grep -o '"secret":"[^"]*' /quickstatements/data/qs-oauth.json | grep -o '[^"]*$')
 
-    envsubst < /templates/oauth.ini > /quickstatements/data/oauth.ini
-fi
+export OAUTH_CONSUMER_KEY
+export OAUTH_CONSUMER_SECRET
+
+envsubst < /templates/oauth.ini > /quickstatements/data/oauth.ini
+
